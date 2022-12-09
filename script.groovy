bafe7b8ec1b0e5c7adbc28502092b9e3780f1ef9
NarayanAdithya <narayanadithya1234@gmail.com>
2022-12-09 13:02:08 +0530

    Updated JenkinsFile

--- script.groovy
+++ script.groovy
@@ -12,7 +12,7 @@ def testApp() {
 def deployApp() {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-adina', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-        sh "docker push narayanadithya/nanajanashia:jenkins1.0"
+        sh "docker push narayanadithya/nanajanashia:jenkins-${BRANCH_NAME}-1.0"
     }
 }
 
